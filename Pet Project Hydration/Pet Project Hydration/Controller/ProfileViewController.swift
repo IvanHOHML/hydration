1b84e2c6f5223e8e789b9239897a84cb1ec723a3
IvanHOHML <45387540+IvanHOHML@users.noreply.github.com>
2020-09-01 16:15:40 +0800

    New Files
    Basic ViewController and Segue set-up

---
+++ Pet Project Hydration/Pet Project Hydration/Controller/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Pet Project Hydration
+//
+//  Created by  Ho Ivan on 9/1/20.
+//  Copyright © 2020  Ho Ivan. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Pet Project Hydration/Pet Project Hydration/Controller/ShowViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pet Project Hydration
+//
+//  Created by  Ho Ivan on 9/1/20.
+//  Copyright © 2020  Ho Ivan. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ShowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
